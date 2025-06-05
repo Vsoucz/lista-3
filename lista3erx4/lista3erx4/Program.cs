b7d649a43ce1610dd70a7a08ae459fa846a0981d
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-06-04 22:09:59 -0300

    Add files via upload

---
+++ lista3erx4/lista3erx4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3erx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
+        }
+    }
+}
