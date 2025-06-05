ecc5d47ab984fd36e9fef864b17afada0bdc3dc2
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-06-04 22:10:33 -0300

    Add files via upload

---
+++ lista3erx5/lista3erx5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3erx5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            do
+            {
+                Console.Write("Digite um valor positivo: ");
+                num = double.Parse(Console.ReadLine());
+            } while (num <= 0);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
+        }
+    }
+}
